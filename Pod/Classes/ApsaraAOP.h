36a5eb89d7fe10f54053a10b2bc05f9ad7830419
Erik Sundin <erik@eriksundin.se>
2014-11-14 15:15:27 +0100

    Added framework header file

---
+++ Pod/Classes/ApsaraAOP.h
@@ -0,0 +1,10 @@
+#import "APAspectManager.h"
+
+#import "APAdvice.h"
+
+#import "APPointcutAdvisor.h"
+#import "APPointcut.h"
+#import "APBlockMatchingPointcut.h"
+
+#import "APJoinPoint.h"
+#import "APProceedingJoinPoint.h"
